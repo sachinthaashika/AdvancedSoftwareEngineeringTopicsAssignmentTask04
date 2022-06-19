54b67edfaf72e30971a17ac870beb9f40d374ddd
sachinthaashika <86411401+sachinthaashika@users.noreply.github.com>
2022-06-19 22:55:59 +0530

    Reflection 01

---
+++ Reflection01.java
@@ -0,0 +1,14 @@
+package Reflection;
+/*
+ * @author Sachintha Ashika Bandara Yapa
+ */
+public class Reflection01 {
+
+  //create an instance Simple and prints it out
+  public static void main(String[] args) {
+    Square s = new Square(20,20);
+    System.out.println("s=" + s);
+  }
+}
+
+
